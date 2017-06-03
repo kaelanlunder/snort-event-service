0ba06f8b3e7e285cd1291268f5ea978dc6181444
kmlunder <klunder98@gmail.com>
2017-06-03 10:31:06 -0400

    Update service_control.rb

--- service_control.rb
+++ service_control.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 # Ensure this file is placed in the same directory as snort_event_receiver.rb
+# To start the service run the comand 'ruby serverice_control.rb start'
 require 'daemons'
 
 Daemons.run('snort_event_retreiver.rb')
