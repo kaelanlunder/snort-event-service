96c87cd40ae1e8e7631de8ae3d4a8c9a1f780d96
kmlunder <klunder98@gmail.com>
2017-06-03 10:25:40 -0400

    Add files via upload

---
+++ service_control.rb
@@ -0,0 +1,3 @@
+require 'daemons'
+
+Daemons.run('snort_event_retreiver.rb')
---
+++ snort_event_retreiver.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+require 'mysql'
+require 'json'
+require 'rest-client'
+require 'yaml'
+
+begin
+  while(true)
+    pwd = File.expand_path(File.dirname(__FILE__))
+    config = YAML.load_file("#{pwd}/config.yml")
+    con = Mysql.new config['host'], config['user'], config['password'], config['database']
+    rs = con.query "SELECT * FROM event WHERE abuse_sent IS NULL"
+    rs.each_hash do |row|
+       timestamp = row['timestamp']
+       cid = row['cid']
+       RestClient.post "http://localhost:3000/snort_event_collectors", row.to_json, {content_type: :json, accept: :json}
+       update = con.query "UPDATE event set abuse_sent = 'Y' where timestamp = '#{timestamp}' and cid = '#{cid}'"
+    end      
+  end
+rescue Mysql::Error => e
+    puts e    
+ensure
+    con.close if con
+end
